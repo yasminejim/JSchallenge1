1e047b2e09e821f7614413b671db4b84e0928fb6
yasminejim <94918510+yasminejim@users.noreply.github.com>
2022-01-10 22:11:48 -0500

    Update very.hard.js

--- very.hard.js
+++ very.hard.js
@@ -7,22 +7,22 @@ const num2 = parseFloat(prompt('Please enter second number: '));
 switch(operator) {
     
     case '+':
-        result = parsefloat(num1) + parsefloat(num2);
+        result = parseFloat(num1) + parseFloat(num2);
         console.log(`${num1} + ${num2} = ${result}`);
          break;
 
     case '-':
-        result = parsefloat(num1) - parsefloat(num2);
+        result = parseFloat(num1) - parseFloat(num2);
         console.log(`${num1} - ${num2} = ${result}`);
         break;
 
     case '*':
-        result = parsefloat(num1) * parsefloat(num2);
+        result = parseFloat(num1) * parseFloat(num2);
         console.log(`${num1} * ${num2} = ${result}`);
         break;
 
     case '/':
-        result = parsefloat(num1) / parsefloat(num2);
+        result = parseFloat(num1) / parseFloat(num2);
         console.log(`${num1} / ${num2} = ${result}`);
         break;
 
